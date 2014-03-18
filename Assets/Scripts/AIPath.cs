06bfe068ff8dc000aec23e9f434263b043f8ab7b
Chase Basich <chasebasich@gmail.com>
2014-03-18 13:57:39 -0700

    Started Repathing
    So far, it isn't working. I've commented out my changes for now, but
    will continue to work on it

--- Assets/Scripts/AIPath.cs
+++ Assets/Scripts/AIPath.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class AIPath : MonoBehaviour {
 	private NavMeshAgent agent;
 	public Vector3 destination;
+	private NavMeshPath tempPath;
+	private Vector3 finalDestination;
+	private bool onTempPath;
 	// Use this for initialization
 
 	private const float MAX_SEARCH = 100.0f;
@@ -21,18 +24,44 @@ public class AIPath : MonoBehaviour {
 		agent.updateRotation = true;
 		destination = this.gameObject.transform.position;
 		agent.SetDestination (destination);
+		onTempPath = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (agent.remainingDistance < 3 && Network.isServer) {
-			destination = getNewDestination();
-			agent.SetDestination (destination);
+		if (!onTempPath){
+			if (agent.remainingDistance < 5 && Network.isServer) {
+				destination = getNewDestination();
+				agent.SetDestination (destination);
+			}
 		}
+	/*	else{
+			if (agent.remainingDistance < .2 && Network.isServer){
+				destination = finalDestination;
+				onTempPath = false;
+				agent.SetDestination(destination);
+			}
+		}*/
 		bool death = GetComponent<AIControlScript> ().death;
 		if (death){
 			agent.Stop();
 		}
+
+		/*NavMeshHit hit;
+
+
+		if (agent.Raycast (agent.nextPosition + agent.velocity * 2, out hit)){
+			if( hit.distance < 2 && Mathf.Abs (Vector3.Dot (hit.normal.normalized, agent.velocity.normalized)) < .5) {
+				Debug.Log (hit.mask);
+				onTempPath = true;
+				finalDestination = agent.destination;
+				Vector3 offset = Quaternion.AngleAxis (-45, agent.transform.up) * agent.velocity * 2;
+				Vector3 target = agent.transform.position + offset;
+
+				NavMesh.SamplePosition(target, out hit, MAX_SEARCH, 1);
+				agent.SetDestination(hit.position);
+			}
+		}*/
 	}
 	
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
